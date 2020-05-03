45dc72d4ce62c631c5c6fd3d7fd524058b4b99e5
ForiDunk <szabolcs.forreiter@gmail.com>
2020-05-03 13:27:25 +0300

    fixes header

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -5,6 +5,8 @@ import { ReactComponent as Instagram } from './icon-instagram.svg';
 import { ReactComponent as Mail } from './icon-mail.svg';
 import headerBackground from './header.svg';
 
+const openTo = (link) => window.open(link, '_blank');
+
 export default () => (
   <div className={styles.home}>
     <div
@@ -16,9 +18,25 @@ export default () => (
         I am Doina, an aspiring UX/UI Designer based in Brasov, Romania.
       </div>
       <div className={styles.socials}>
-        <Linkedin className={styles.social} />
-        <Instagram className={styles.social} />
-        <Mail className={styles.social} />
+        <Linkedin
+          onClick={() =>
+            openTo(
+              'https://www.linkedin.com/in/lisneac-forreiter-doina-b320bb183/'
+            )
+          }
+          className={styles.social}
+        />
+        <Instagram
+          onClick={() => openTo('https://www.instagram.com/marley_fox92/')}
+          className={styles.social}
+        />
+        <a
+          href='mailto:doinalisneac92@gmail.com'
+          target='_blank'
+          rel='noopener noreferrer'
+        >
+          <Mail className={styles.social} />
+        </a>
       </div>
     </div>
   </div>
