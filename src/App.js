811ac188167cbc1da96e88744b7ed6d807e4820f
ForiDunk <szabolcs.forreiter@gmail.com>
2020-05-16 18:39:47 +0300

    particles

--- src/App.js
+++ src/App.js
@@ -5,23 +5,46 @@ import RoadBikes from './pages/RoadBikes/RoadBikes';
 import ChefRecipes from './pages/ChefRecipes/ChefRecipes';
 import Home from './pages/Home/Home';
 import Footer from './components/Footer/Footer';
+import Particles from 'react-particles-js';
+const particlesOptions = {
+  particles: {
+    number: {
+      value: 200,
+      density: {
+        enable: true,
+        value_are: 800,
+      },
+    },
+  },
+  interactivity: {
+    events: {
+      onhover: {
+        enable: true,
+        mode: 'repulse',
+      },
+    },
+  },
+};
 
 export default () => (
-  <Router>
-    <Switch>
-      <Route path='/maynooth'>
-        <Maynooth />
-      </Route>
-      <Route path='/road-bikes'>
-        <RoadBikes />
-      </Route>
-      <Route path='/chef-recipes'>
-        <ChefRecipes />
-      </Route>
-      <Route exact path='/'>
-        <Home />
-      </Route>
-    </Switch>
-    <Footer />
-  </Router>
+  <>
+    <Particles className='particles' params={particlesOptions} />
+    <Router>
+      <Switch>
+        <Route path='/maynooth'>
+          <Maynooth />
+        </Route>
+        <Route path='/road-bikes'>
+          <RoadBikes />
+        </Route>
+        <Route path='/chef-recipes'>
+          <ChefRecipes />
+        </Route>
+        <Route exact path='/'>
+          <Home />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  </>
 );
