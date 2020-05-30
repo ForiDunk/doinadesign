cdd89f4e4baf63401c165e85d62a8f7e9af140bc
ForiDunk <ForiDunk@192-168-0-182.rdsnet.ro>
2020-05-30 23:52:14 +0300

    change text

--- src/pages/Home/About.jsx
+++ src/pages/Home/About.jsx
@@ -22,33 +22,34 @@ export default ({ setActive }) => {
   window.addEventListener('orientationchange', checIfMedium);
 
   return (
-    <div id='about' className={styles.about}>
+    <div id="about" className={styles.about}>
       <div className={styles.title}>ABOUT ME</div>
       <div className={styles.container}>
         <div className={styles.text}>
           {isMedium && (
-            <img className={styles.avatar} src={avatarSrc} alt='Avatar of me' />
+            <img className={styles.avatar} src={avatarSrc} alt="Avatar of me" />
           )}
           <p>
-            My name is Doina Lisneac-Forreiter, a 27 years old aspiring UI/UX
+            My name is Doina Lisneac-Forreiter, a 27-year-old aspiring UI/UX
             Designer. I was always a very curious person, eager to learn a lot
-            and try new things. I have been working as an Aesthetician for
-            almost 2 years. For the last 2.5 years I’ve worked in Accounting and
-            Customer Support fields, but I was unhappy about my career and felt
-            that this is not my path, that is why I have decided to open a new
-            door in my life and started to learn web design.
+            and try new things. I’ve worked as an Aesthetician for almost 2
+            years, in Accounting and Customer Support fields for 2.5 years, but
+            I was unhappy about my career. As my moto is: “If you don’t like it,
+            change it”, I’ve decided to open a new door in my life and started
+            learning web design.
           </p>
           <p>
-            Why did I choose this particular profession? First of all, I’ve been
-            inspired by my husband who is a web developer. He is so passionate
-            about this field and he has been my best support in starting this
-            journey. Second of all, I has been searching for a creative and
-            always changing career because I have been inloved with art and
-            technology since childhood.
+            Why did I choose this particular profession? First of all, I have
+            been searching for a creative and always changing field because of
+            my passion for art and my curious nature. Second of all, I’ve been
+            inspired by my husband who is a web developer and who supported me a
+            lot in starting this journey. I believe that my willingness to learn
+            and hard work will help me to achieve my goals of becoming a great
+            designer.
           </p>
         </div>
         {!isMedium && (
-          <img className={styles.avatar} src={avatarSrc} alt='Avatar of me' />
+          <img className={styles.avatar} src={avatarSrc} alt="Avatar of me" />
         )}
       </div>
     </div>
